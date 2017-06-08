e57dc1fbc24d4f2a218dadeb9fd2876d7ab86f24
Phillip Creglow <phillipcreglow@msn.com>
2017-06-07 21:45:34 -0400

    Setup daily traffic chart in app.js, apply styles to daily traffic chart

--- app.js
+++ app.js
@@ -29,3 +29,47 @@ var traffic = new Chart(ctx, {
         }
     }
 });
+
+// Daily Traffic - Chart.js //
+
+var ctx = document.getElementById("daily-traffic").getContext('2d');
+var traffic = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ["Su", "M", "Tu", "W", "Th", "F", "Sa"],
+        datasets: [{
+            label: '# of site visitors (millions)',
+            data: [2.5, 2.9, 1.7, 1.5, 1.9, 1.4, 1.1],
+            backgroundColor: [
+                'rgba(116, 120, 186, 0.3)',
+                'rgba(116, 120, 186, 0.3)',
+                'rgba(116, 120, 186, 0.3)',
+                'rgba(116, 120, 186, 0.3)',
+                'rgba(116, 120, 186, 0.3)',
+                'rgba(116, 120, 186, 0.3)',
+                'rgba(116, 120, 186, 0.3)'
+            ],
+            borderColor: [
+                'rgb(116, 120, 186)',
+                'rgb(116, 120, 186)',
+                'rgb(116, 120, 186)',
+                'rgb(116, 120, 186)',
+                'rgb(116, 120, 186)',
+                'rgb(116, 120, 186)',
+                'rgb(116, 120, 186)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero:true
+                }
+            }]
+        }
+    }
+});
+
+// Mobile Users - Chart.js //
